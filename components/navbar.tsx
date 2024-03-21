8e5073c1c59d7e6af433daa1a11ab221a383b48f
evie-8 <neouma29@gmail.com>
2024-03-21 18:31:59 +0300

    adding shadow to the navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -11,7 +11,7 @@ export const revalidate = 0;
 const NavBar =  async() => {
   const categories = await getCategories()
   return (
-    <div className='bg-white sticky top-0 left-0  z-40'>
+    <div className='bg-white sticky top-0 left-0  z-40 shadow-md'>
       <div className='border-b border-light-grey'>
         <Container>
             <div className='px-4 flex h-16 items-center'>
